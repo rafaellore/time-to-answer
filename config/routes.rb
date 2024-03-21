a6250eb5ba07e32dfe23885a6643805b8524a4ff
rafaellore <birdneon3@gmail.com>
2024-03-21 18:09:59 -0300

    feat(subject_controller): adds initial subject controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
     resources :admins # Administradores
+    resources :subjects # Assuntos/Áreas
   end
   devise_for :admins
   get 'welcome/index'
