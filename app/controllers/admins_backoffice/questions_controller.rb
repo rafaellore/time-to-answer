2de7e241dd9fa139f2db4a1d698027319114fc8d
rafaellore <birdneon3@gmail.com>
2024-03-25 11:20:41 -0300

    refactor(questions_controller): fix N1 problem performance

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -4,7 +4,9 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   
 
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject)
+                         .order(:description)
+                         .page(params[:page])
   end
 
   def new
